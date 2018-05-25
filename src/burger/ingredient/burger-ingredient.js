3ad33c77f48cbed5e12701feb9565e5b5f98989d
irohitb <irohitbhatia@gmail.com>
2018-05-26 04:22:41 +0530

    burger-ingredient.s and burger-ingredient.css added

---
+++ src/burger/ingredient/burger-ingredient.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import classes from './Burger-Ingredient.css'
+
+const BurgerIngredients = (props) => {
+  let ingredient = null;
+
+  switch (props.type) {
+    case ('bread-bottom');
+      ingredient = <div className={classes.BreadBotton};</div>
+      console.log(typeof ingredient);
+    break;
+    case ('bread-top');
+      ingrident = (
+        <div className = {classes.BreadTop}>
+          <div className = {classes.Seeds1}></div>
+          <div className = {classes.Seeds2}></div>
+        </div>
+        );
+        console.log(typeof ingrident)
+    break;
+    case ('meat');
+      ingrident = <div className = {classes.Meat}> </div>
+    break;
+    case ('Cheese');
+      ingrident = <div className = {classes.Cheese}> </div>
+    break;
+    case ('Salad');
+      ingrident = <div className = {classes.Salad}> </div>
+    break;
+    case ('Bacon');
+      ingrident = <div className = {classes.Bacon}> </div>
+    break;
+    default:
+      ingrident = null;
+    }
+
+    return ingredient;
+
+};
+
+export default BurgerIngredients;
