bd6a77fa742f0d2ed0af6fd6012f6cec1cc76910
irohitb <irohitbhatia@gmail.com>
2018-05-30 07:41:28 +0530

    State and dynamic content start

--- src/burger/burger.js
+++ src/burger/burger.js
@@ -4,14 +4,46 @@ import BurgerIngredients from './ingredient/burger-ingredient.js'
 
 
 const burger = (props) => {
+
+/*
+  let transformedIngrident = Object.keys(props.ingredient) //["salad", "bacon", "cheese", "meat"]
+  transformedIngrident = transformedIngrident.map(igKey => { //igKey will pass string which will be mapped like salad then bacon then cheese and then meat
+
+  //This will tell the value corresponding to the give string of object for example in case of Salad it will show 1
+
+  //[undefined] -> This is because we have  one salad in Ingridents
+  //[undefined] -> This is because we have  one bacon
+  //[undefined, undefined, undefined] This is because we have  3 bacon
+  //[undefined, undefined] This is because we have two meat
+    return [...Array(props.ingredient[igKey])].map((_, i) => {
+      console.log(transformedIngrident)
+        return <BurgerIngredients type={igKey} key={i} />
+    //This will first create an empty array and the map used second time will map each ingrident into it with type and key
+    })
+  })
+*/
+
+let transformedIngrident = Object.keys(props.ingredient)
+var DisplayIngre = [] //this should be an array
+var i;
+var j;
+
+for (i=0; i<transformedIngrident.length; i++){
+  let keyValue = props.ingredient[transformedIngrident[i]]
+  let igKey = transformedIngrident[i]
+    for ( j=0; j<keyValue; j++) {
+DisplayIngre.push(<BurgerIngredients type={igKey} key={i+j} />);
+  }
+}
+
+
+
+
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredients type="bread-top"/>
-      <BurgerIngredients type="meat"/>
-      <BurgerIngredients type="Cheese"/>
-      <BurgerIngredients type="Salad"/>
-      <BurgerIngredients type="Bacon"/>
-
+      {DisplayIngre}
       <BurgerIngredients type="bread-bottom"/>
 
     </div>
--- src/container/BurgerBuilder/burgerBuilder.js
+++ src/container/BurgerBuilder/burgerBuilder.js
@@ -3,10 +3,21 @@ import Aux from '../../HOC/Aux.js'
 import Burger from '../../burger/burger.js'
 
 class BurgerBuilder extends Component {
+state = {
+    ingredient: {
+      Salad: 1,
+      Bacon: 1,
+      Cheese: 1,
+      meat: 2
+    }
+  }
+
+
+
   render () {
     return (
         <Aux>
-          <Burger />
+          <Burger ingredient={this.state.ingredient}/>
           <div> Build controls </div>
         </Aux>
     )
